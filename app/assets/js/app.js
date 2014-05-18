49c0ac7357d0d752e524363ffd13be449306dcc9
simonsweeney <hello@simonsweeney.me>
2014-05-18 20:54:22 +0100

    push for eoghan

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -8,10 +8,15 @@
 }; */
 
 
-	$(document).ready(function() {
-		$(".fancybox")
-		.attr('rel', 'gallery')
-		.fancybox({
-	        padding : 0
-	    });
-	});
+$(document).ready(function() {
+	$(".fancybox")
+	.attr('rel', 'gallery')
+	.fancybox({
+        padding : 0
+    });
+});
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
